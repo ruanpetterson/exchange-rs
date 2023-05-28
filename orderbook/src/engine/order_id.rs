42cc2ee4949602fcc57bb084a794ff9e499e0fcf
Ruan Petterson <ruan@petterson.eng.br>
2023-05-28 13:04:24 -0300

    fix: serde imports

--- orderbook/src/engine/order_id.rs
+++ orderbook/src/engine/order_id.rs
@@ -1,9 +1,6 @@
-#[cfg(feature = "serde")]
-use serde::{Deserialize, Serialize};
-
 #[repr(transparent)]
 #[derive(Clone, Copy, Debug, Hash, PartialEq, Eq)]
-#[cfg_attr(feature = "serde", derive(Serialize, Deserialize))]
+#[cfg_attr(feature = "serde", derive(serde::Serialize, serde::Deserialize))]
 pub struct OrderId(u64);
 
 impl OrderId {
