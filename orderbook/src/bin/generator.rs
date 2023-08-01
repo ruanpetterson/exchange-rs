d2a194683585844f5735c39065e9a9d72f11985d
Ruan Petterson <ruan@petterson.eng.br>
2023-08-01 19:48:07 -0300

    feat(bin): accept value from stdin (#32)

--- orderbook/src/bin/generator.rs
+++ orderbook/src/bin/generator.rs
@@ -1,43 +1,38 @@
-use std::path::Path;
-use std::{fs::File, io::Result};
+use std::io;
+use std::io::{Result, Write};
 
 use compact_str::{format_compact, CompactString};
 use orderbook_core::OrderSide;
 use orderbook_types::OrderRequest;
 use rand::Rng;
 
-const N: usize = 7_500_000;
+const N: usize = 10_000_000;
 
 fn main() -> Result<()> {
-    let path = Path::new("./orders.json");
-    let file = File::options()
-        .read(true)
-        .write(true)
-        .create(true)
-        .truncate(true)
-        .open(path)?;
-
     let mut rng = rand::thread_rng();
     let orders = (1..=N)
-        .map(|i| match rng.gen_range(0..1000) {
+        .map(|i| match rng.gen_range(0..1_000) {
             0 => OrderRequest::Delete {
                 order_id: format_compact!("{}", rng.gen_range(1..=i as u64)),
             },
             _ => OrderRequest::Create {
-                account_id: format_compact!("{}", rng.gen_range(1..10)),
-                amount: rng.gen_range(1000..2000).into(),
+                account_id: format_compact!("{}", rng.gen_range(1..100)),
+                amount: rng.gen_range(100..10_000).into(),
                 order_id: format_compact!("{}", i as u64),
                 pair: CompactString::new_inline("BTC/USDC"),
-                limit_price: rng.gen_range(1000..2000).into(),
+                limit_price: rng.gen_range(100..10_000).into(),
                 side: match rng.gen_range(0..2) {
                     0 => OrderSide::Ask,
                     _ => OrderSide::Bid,
                 },
             },
         })
-        .collect::<Vec<_>>();
+        .filter_map(|order| serde_json::to_string(&order).ok());
 
-    serde_json::to_writer(file, &orders)?;
+    let mut stdout = io::stdout();
+    for order in orders {
+        writeln!(stdout, "{}", order)?;
+    }
 
     Ok(())
 }
