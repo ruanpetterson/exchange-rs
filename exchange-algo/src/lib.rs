9e055ad58b46d625de7cdd2c145fe534f62de58c
Ruan Petterson <ruan@petterson.eng.br>
2023-10-31 11:25:49 -0300

    fix(algo): fill-or-kill orders (#45)

--- exchange-algo/src/lib.rs
+++ exchange-algo/src/lib.rs
@@ -73,26 +73,43 @@ mod policy {
     }
 
     struct FillOrKill;
-    impl<E: Exchange + ExchangeExt> Policy<E::Order, E> for FillOrKill {
+    impl<E: Exchange> Policy<E::Order, E> for FillOrKill {
         #[inline]
         fn enforce(&self, incoming_order: &mut E::Order, exchange: &E) {
-            let peek_all = |incoming_order: &E::Order| {
-                exchange
-                    .iter(&incoming_order.side().opposite())
-                    .take_while(|order| order.matches(&incoming_order))
-                    .map(|order| order.remaining())
-                    .reduce(|curr, acc| curr + acc)
-                    .unwrap_or(Zero::zero())
-            };
-
             if incoming_order.is_fill_or_kill()
-                && incoming_order.remaining() >= peek_all(&incoming_order)
+                && !FillOrKill::can_fill(incoming_order, exchange)
             {
                 // The exchange should possess a sufficient number of orders to
                 // execute an all-or-none order; otherwise, the all-or-none
                 // order must be cancelled.
                 incoming_order.cancel();
             }
+        }
+    }
+
+    impl FillOrKill {
+        /// Returns if `incoming_order` can be completely filled within given
+        /// exchange.
+        ///
+        /// `incoming_order` is the order you want to check if it can be filled,
+        /// and `exchange` is the orderbook that we'll use to compare against
+        /// the given order.
+        #[inline]
+        fn can_fill<E: Exchange>(
+            incoming_order: &E::Order,
+            exchange: &E,
+        ) -> bool {
+            exchange
+                .iter(&incoming_order.side().opposite())
+                .take_while(|order| {
+                    // Gather only the orders that are compatible to the
+                    // `incoming_order`.
+                    order.matches(incoming_order)
+                })
+                .map(|order| order.remaining())
+                .reduce(|curr, acc| curr + acc)
+                .unwrap_or(Zero::zero())
+                >= incoming_order.remaining()
         }
     }
 
