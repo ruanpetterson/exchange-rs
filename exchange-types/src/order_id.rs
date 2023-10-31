ae62602d37bbeb53663aa6c4d648aa8ab66e1328
Ruan Petterson <ruan@petterson.eng.br>
2023-10-31 14:17:13 -0300

    feat!: use `Uuid` as unique identifier

--- exchange-types/src/order_id.rs
+++ exchange-types/src/order_id.rs
@@ -1,11 +1,32 @@
+use uuid::Uuid;
+
 #[repr(transparent)]
 #[derive(Clone, Copy, Debug, Hash, PartialEq, Eq, PartialOrd, Ord)]
 #[cfg_attr(feature = "serde", derive(serde::Serialize, serde::Deserialize))]
-pub struct OrderId(u64);
+pub struct OrderId(Uuid);
 
 impl OrderId {
     #[inline]
-    pub fn new(order_id: u64) -> Self {
-        Self(order_id)
+    pub fn new(uuid: Uuid) -> Self {
+        Self(uuid)
+    }
+
+    #[inline]
+    pub fn random() -> Self {
+        Self(Uuid::new_v4())
+    }
+}
+
+impl From<Uuid> for OrderId {
+    #[inline]
+    fn from(uuid: Uuid) -> Self {
+        Self::new(uuid)
+    }
+}
+
+impl From<&Uuid> for OrderId {
+    #[inline]
+    fn from(uuid: &Uuid) -> Self {
+        Self::new(*uuid)
     }
 }
--- exchange/tests/fill_or_kill.rs
+++ exchange/tests/fill_or_kill.rs
@@ -6,13 +6,13 @@ use tap::{Pipe, Tap};
 #[test]
 fn valid_match() {
     let mut exchange = Orderbook::new().tap_mut(|exchange| {
-        let order_id = OrderId::new(1);
+        let order_id = OrderId::random();
         let limit_order = Order::new_limit(order_id, OrderSide::Ask, 100, 100);
 
         assert!(exchange.matching(limit_order).is_ok());
     });
 
-    let fill_or_kill = OrderId::new(1).pipe(|order_id| {
+    let fill_or_kill = OrderId::random().pipe(|order_id| {
         Order::new(
             order_id,
             OrderSide::Bid,
@@ -33,13 +33,13 @@ fn valid_match() {
 #[test]
 fn invalid_match() {
     let mut exchange = Orderbook::new().tap_mut(|exchange| {
-        let order_id = OrderId::new(1);
+        let order_id = OrderId::random();
         let limit_order = Order::new_limit(order_id, OrderSide::Ask, 100, 10);
 
         assert!(exchange.matching(limit_order).is_ok());
     });
 
-    let fill_or_kill = OrderId::new(1).pipe(|order_id| {
+    let fill_or_kill = OrderId::random().pipe(|order_id| {
         Order::new(
             order_id,
             OrderSide::Bid,
