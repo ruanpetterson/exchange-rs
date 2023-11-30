efcab7c96183772759fb39a2edc71936f26d92d4
Ruan Petterson <ruan@petterson.eng.br>
2023-11-30 20:08:41 -0300

    poc: async runtime

--- exchange/src/main.rs
+++ exchange/src/main.rs
@@ -1,7 +1,4 @@
-use std::io::BufRead;
-use std::io::BufReader;
 use std::path::PathBuf;
-use std::sync::mpsc;
 use std::time::Instant;
 
 use anyhow::Result;
@@ -10,6 +7,10 @@ use compact_str::CompactString;
 use exchange_core::ExchangeExt;
 use exchange_rt::Engine;
 use owo_colors::OwoColorize;
+use tap::Pipe;
+use tokio::io::{AsyncBufReadExt as _, BufReader};
+use tokio::sync::mpsc;
+use tokio::task::JoinSet;
 
 #[derive(Parser)]
 #[clap(author, version, about)]
@@ -27,56 +28,59 @@ struct Args {
     output: Option<Output>,
 }
 
-fn main() -> Result<()> {
+#[tokio::main]
+async fn main() -> Result<()> {
     let args = Args::parse();
+    let mut set = JoinSet::new();
 
-    let (tx, rx) = mpsc::sync_channel(65_536);
+    let (raw_tx, raw_rx) = flume::bounded(65_536);
+    let (deserialized_tx, mut deserialized_rx) = mpsc::channel(65_536);
 
-    std::thread::spawn(move || -> Result<()> {
-        let mut buf_read: Box<dyn BufRead> =
-            match &args.input.unwrap_or_default() {
-                Input::File(path) => {
-                    let file = std::fs::File::open(path)?;
-                    Box::new(BufReader::new(file))
-                }
-                Input::Stdin => {
-                    let stdin = std::io::stdin();
-                    Box::new(BufReader::new(stdin))
-                }
-            };
-
-        let mut buf = String::with_capacity(4096);
-        while let Ok(_) = buf_read.read_line(&mut buf) {
-            let order = serde_json::from_str(&buf);
-            buf.clear();
-            match order {
-                Err(error) => {
-                    if error.is_eof() {
-                        break;
-                    }
-
-                    eprintln!("{error}");
-                }
-                Ok(order) => tx.send(order)?,
+    set.spawn(async move {
+        let reader = match &args.input.unwrap_or_default() {
+            Input::File(path) => {
+                let file = tokio::fs::File::open(path).await?;
+                tokio_util::either::Either::Left(file)
+            }
+            Input::Stdin => {
+                let stdin = tokio::io::stdin();
+                tokio_util::either::Either::Right(stdin)
             }
         }
+        .pipe(BufReader::new);
 
-        Ok(())
+        let mut lines = reader.lines();
+        while let Some(line) = lines.next_line().await? {
+            raw_tx.send_async(line).await?;
+        }
+
+        Ok::<_, anyhow::Error>(())
+    });
+
+    set.spawn(async move {
+        while let Ok(raw_order) = raw_rx.recv_async().await {
+            let Ok(deserialized_order) = serde_json::from_str(&raw_order)
+            else {
+                continue;
+            };
+
+            deserialized_tx.send(deserialized_order).await?;
+        }
+
+        Ok::<_, anyhow::Error>(())
     });
 
     let mut engine = Engine::new(&args.pair);
 
     let mut i = 0.0f64;
     let begin = Instant::now();
-    while let Ok(order) = rx.recv() {
+    while let Some(order) = deserialized_rx.recv().await {
         if let Err(err) = engine.process(order) {
             eprintln!("something went wrong: {}", err);
         };
         i += 1.0;
     }
-    let end = Instant::now();
-
-    let elapsed = end - begin;
+    let elapsed = begin.elapsed();
     let (ask_length, bid_length) = engine.orderbook().len();
 
     eprintln!(
