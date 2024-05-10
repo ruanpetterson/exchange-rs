d003ce33ef0c54ec85f4053c23a43a7f7726cb3a
Ruan Petterson <ruan@petterson.eng.br>
2024-05-10 19:21:15 -0300

    test: introduce snapshot for integration tests

--- matching-engine/matching-engine/tests/integration_test.rs
+++ matching-engine/matching-engine/tests/integration_test.rs
@@ -1,3 +1,5 @@
+use assert2::assert;
+use assert2::let_assert;
 use exchange_core::Asset;
 use exchange_core::Trade as _;
 use exchange_types::LimitOrder;
@@ -15,9 +17,26 @@ fn simple_match() {
     let mut ask: LimitOrder = ORDERS[0].try_into().unwrap();
     let mut bid = ORDERS[1];
 
-    assert!(ask.trade(&mut bid).is_ok());
+    let_assert!(Ok(trade) = ask.trade(&mut bid));
     assert!(ask.is_closed());
     assert!(bid.is_closed());
+
+    insta::assert_ron_snapshot! {
+        &trade,
+        {
+            ".taker" => "[uuid]",
+            ".maker" => "[uuid]",
+        },
+        @r###"
+        Trade(
+          taker: "[uuid]",
+          maker: "[uuid]",
+          amount: "100",
+          price: "50000",
+          notional: "5000000",
+        )
+        "###
+    }
 }
 
 #[test]
@@ -25,9 +44,26 @@ fn partial_match() {
     let mut ask: LimitOrder = ORDERS[3].try_into().unwrap();
     let mut bid = ORDERS[2];
 
-    assert!(ask.trade(&mut bid).is_ok());
+    let_assert!(Ok(trade) = ask.trade(&mut bid));
     assert!(!ask.is_closed());
     assert!(bid.is_closed());
+
+    insta::assert_ron_snapshot! {
+        &trade,
+        {
+            ".taker" => "[uuid]",
+            ".maker" => "[uuid]",
+        },
+        @r###"
+        Trade(
+          taker: "[uuid]",
+          maker: "[uuid]",
+          amount: "100",
+          price: "50000",
+          notional: "5000000",
+        )
+        "###
+    }
 }
 
 #[test]
@@ -35,8 +71,25 @@ fn taker_advantage_for_ask() {
     let mut ask: LimitOrder = ORDERS[3].try_into().unwrap();
     let mut bid = ORDERS[2];
 
-    let trade = ask.trade(&mut bid).expect("a sucessful trade");
+    let_assert!(Ok(trade) = ask.trade(&mut bid));
     assert_eq!(trade.price(), ask.limit_price().unwrap());
+
+    insta::assert_ron_snapshot! {
+        &trade,
+        {
+            ".taker" => "[uuid]",
+            ".maker" => "[uuid]",
+        },
+        @r###"
+        Trade(
+          taker: "[uuid]",
+          maker: "[uuid]",
+          amount: "100",
+          price: "50000",
+          notional: "5000000",
+        )
+        "###
+    }
 }
 
 #[test]
@@ -44,8 +97,25 @@ fn taker_advantage_for_bid() {
     let mut bid: LimitOrder = ORDERS[2].try_into().unwrap();
     let mut ask = ORDERS[3];
 
-    let trade = bid.trade(&mut ask).expect("a sucessful trade");
+    let_assert!(Ok(trade) = bid.trade(&mut ask));
     assert_eq!(trade.price(), bid.limit_price().unwrap());
+
+    insta::assert_ron_snapshot! {
+        &trade,
+        {
+            ".taker" => "[uuid]",
+            ".maker" => "[uuid]",
+        },
+        @r###"
+        Trade(
+          taker: "[uuid]",
+          maker: "[uuid]",
+          amount: "100",
+          price: "60000",
+          notional: "6000000",
+        )
+        "###
+    }
 }
 
 #[test]
